f067c015b42fbbf4fe7bf430871f4569a1c644c4
bluheiyy <81110192+bluheiyy@users.noreply.github.com>
2021-04-19 13:54:16 -0400

    Add files via upload
    kjgber jkhkahd jkahw dkjahwdkj hasjkd hakjsd hkajh dkjwh kjdahwjdh vqhju ouaOUALUYGSD KJHaserKHJ HVGASERJKLHG

---
+++ gtr/__resource.lua
@@ -0,0 +1,13 @@
+files {
+
+	'data/vehicles.meta', 
+	'data.carcol.meta',
+	'data/carvariations.meta',
+	'data/handling.meta'
+
+}
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data.carcol.meta'
+data_file 'VEHICAL_VARITATION_FILE' 'data/carvariations.meta'
